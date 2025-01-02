02acb8a90e6ffb549c885a16e626705e42c230ad
Saliya Pathum <infivellc@gmail.com>
2025-01-02 16:29:03 +0530

    update

--- frontend/src/components/home/SigninHome.jsx
+++ frontend/src/components/home/SigninHome.jsx
@@ -15,7 +15,7 @@ const SigninHome = ({ show, onHide, onSigninSuccess, onShowSignup }) => {
   const handleSignin = async () => {
     try {
       console.log('Attempting to sign in with email:', email);
-      const response = await axios.post('https://tapilinq.com/api/login/signin', {
+      const response = await axios.post('https://backend-api.tapilinq.com.com/api/login/signin', {
         email,
         password,
       });
