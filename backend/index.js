063d1d56e68ca1b51d9fc0ed3f018df4a9fc1b42
Saliya Pathum <infivellc@gmail.com>
2025-01-02 14:44:41 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
@@ -6,22 +7,7 @@ const loginRoutes = require('./routes/loginRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
-
-// Set up CORS based on environment
-const allowedOrigins = ['https://tapilinq.com'];  // Your frontend URL for production
-const corsOptions = {
-  origin: (origin, callback) => {
-    // Allow requests from your frontend only
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-
-// Use CORS middleware with dynamic origin for production
-app.use(cors(corsOptions));
+app.use(cors({ origin: '*' }));
 
 // Increase payload limit to handle large image uploads
 app.use(express.json({ limit: '20mb' }));
